52917796925676c05a7fcd0c29a08f59c524e662
13pa22 <32932581+13pa22@users.noreply.github.com>
2019-08-27 00:05:17 -0400

    upd

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ state = {
       e.preventDefault();
       const city = e.target.elements.city.value;
       const country = e.target.elements.country.value;
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}&units=metric`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}&units=metric`);
     
       const data = await api_call.json();
 
